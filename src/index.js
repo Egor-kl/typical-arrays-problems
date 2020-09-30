f7a80bbb421fe75840bbecf9d72b40241fc46d59
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-09-30 22:06:57 +0300

    feat: complete task "array problems"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0
+    }
+    var tmp; 
+    for (var i = array.length - 1; i > 0; i--) {  
+        var counter=0;
+        for (var j = 0; j < i; j++) {
+            if (array[j] > array[j+1]) {
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+                counter++;
+            }
+        }  
+        if(array.length ===0){
+          break;
+        } 
+    }
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0
+    }
+    var tmp; 
+    for (var i = array.length - 1; i > 0; i--) {  
+        var counter=0;
+        for (var j = 0; j < i; j++) {
+            if (array[j] > array[j+1]) {
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+                counter++;
+            }
+        }  
+        if(array.length ===0){
+          break;
+        } 
+    }
+    return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    }
 }
